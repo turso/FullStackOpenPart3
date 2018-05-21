5c83a63e5175f87ff3f65ff21735ff7df1429b56
Oliver Rikkilä <oliver.rikkila@siili.com>
2018-05-21 22:43:26 +0300

    first commit, tehtava 3.1

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+let persons = [
+  {
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1
+  },
+  {
+    name: 'Martti Tienari',
+    number: '040-123456',
+    id: 2
+  },
+  {
+    name: 'Arto Järvinen',
+    number: '040-123456',
+    id: 3
+  },
+  {
+    name: 'Herra huu',
+    number: '040-12334436',
+    id: 4
+  }
+];
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>');
+});
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+});
+
+const port = 3001;
+app.listen(port);
+console.log(`Server running on port ${port}`);
