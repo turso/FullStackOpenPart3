9e739ff89438b10860ce6fb5dcec6438d0619d03
Oliver Rikkilä <oliver.rikkila@siili.com>
2018-05-22 14:38:20 +0300

    tehtava 3.4

--- index.js
+++ index.js
@@ -55,6 +55,13 @@ app.get('/info', (req, res) => {
   res.send(info);
 });
 
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(person => person.id !== id);
+
+  res.status(204).end();
+});
+
 const port = 3001;
 app.listen(port);
 console.log(`Server running on port ${port}`);
