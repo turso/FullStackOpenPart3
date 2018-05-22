b64b79477d3be6eb0f0085bf5c8cab1d98e6a76b
Oliver Rikkilä <oliver.rikkila@siili.com>
2018-05-22 14:11:28 +0300

    tehtava 3.3

--- index.js
+++ index.js
@@ -27,7 +27,6 @@ let persons = [
 const date = new Date();
 
 const listSize = persons.length;
-
 const info = `
 puhelinluettelossa on ${listSize} henkilön tiedot <br><br>
 ${date}
@@ -39,6 +38,17 @@ app.get('/', (req, res) => {
 
 app.get('/api/persons', (req, res) => {
   res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
 });
 
 app.get('/info', (req, res) => {
