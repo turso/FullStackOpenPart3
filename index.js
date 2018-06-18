552c29bffda735d907d7ac3ed69d1f93d469ccf7
Oliver Rikkilä <oliver.rikkila@siili.com>
2018-06-18 14:22:53 +0300

    tehtava 3.10 valmis

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 app.use(cors());
+app.use(express.static('build'));
 
 app.use(bodyParser.json());
 app.use(morgan(':method :url :res :status :response-time[4]'));
